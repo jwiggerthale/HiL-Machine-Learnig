560aaa0fd1aa78a628071d7f07581e9426ab9a6b
jwiggerthale <155433120+jwiggerthale@users.noreply.github.com>
2025-07-06 07:23:45 +0200

    Update visualize_uncertainties.py

--- XAI/Monte Carlo Dropout/visualize_uncertainties.py
+++ XAI/Monte Carlo Dropout/visualize_uncertainties.py
@@ -2,8 +2,20 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
+"""
+Function which takes list of uncertanties and normalizes it to range 0 - 1
+call with: 
+    uncertainties: pandas series of uncertainties
+
+Returns: 
+    uncertainties_norm: pandas series of normalized uncertainties
+"""
+def uncertainty_norm(uncertainties):
+    uncertainties_norm = (uncertainties - uncertainties.min()) / (uncertainties.max() - uncertainties.min())
+    return uncertainties_norm
 
 uncertainties = pd.read_csv('ModelUncertainties.csv')
 uncertainties['is_correct'] = uncertainties['class_pred'] == uncertainties['true_class']
+uncertainties['class_un_norm'] = uncertainty_norm(uncertainties['class_un'])
 
 px.box(uncertainties, x = 'is_correct', y ='class_un', title = 'Uncertainties of Xception Model')
